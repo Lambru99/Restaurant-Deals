4b7fd797903853d777712504335a7f164b75dd11
Dumonu <jacobcherry36@gmail.com>
2017-03-04 15:23:02 -0600

    Renamed directionsAPI to be a js file. Added safe API key handling.

---
+++ API/directionsAPI.js
@@ -0,0 +1,11 @@
+var fs = require('fs');
+var mapsApi = require('@google/maps');
+var mapsApiClient = null;
+fs.readFile('~/.apiKeys/googleMaps', 'utf8', function(err, data) {
+  if(err) {
+    return console.log(err);
+  }
+  mapsApiClient = mapsApi.createClient({
+    key: data
+  });
+});
