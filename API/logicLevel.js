e1e4aeb3ea53d6582db89294c1922f1edecac36d
Dumonu <jacobcherry36@gmail.com>
2017-03-04 14:12:20 -0600

    Made the return of getRests an array.

--- API/logicLevel.js
+++ API/logicLevel.js
@@ -3,11 +3,14 @@ var httpLevel = require('./httpLevel.js');
 exports.getRests = function(latitute, longitude, radius, callback)
 {
   var temp = {
-    name: "Bob's Bob Shack",
-    image_url: 'http://mylolface.com/assets/faces/happy-smile.jpg',
-    distance: 9.3,
-    deal: "Free Bobs until the End of Time",
-    hours: "-1:00 AM to 13:00 PM"
+    restaurants: [
+    {
+      name: "Bob's Bob Shack",
+      image_url: 'http://mylolface.com/assets/faces/happy-smile.jpg',
+      distance: 9.3,
+      deal: "Free Bobs until the End of Time",
+      hours: "-1:00 AM to 13:00 PM"
+    } ]
   }
   callback(temp);
 }
