889beed8c5a3c1295cf30bf648ed6bbb4da49c48
Dumonu <jacobcherry36@gmail.com>
2017-03-04 13:45:52 -0600

    Don't question it.

--- API/logicLevel.js
+++ API/logicLevel.js
@@ -4,7 +4,7 @@ exports.getRests = function(latitute, longitude, radius, callback)
 {
   var temp = {
     name: "Bob's Bob Shack",
-    image-url: "http://vignette4.wikia.nocookie.net/supersmashbrosfanon/images/2/2a/Screen_Shot_2015-05-06_at_8.29.10_PM.png/revision/latest?cb=20150509164120",
+    image-url: "http://vignette4.wikia.nocookie.net/supersmashbrosfanon/images/2/2a/Screen_Shot_2015-05-06_at_8.29.10_PM.png",
     distance: 9.3,
     deal: "Free Bobs until the End of Time",
     hours: "-1:00 AM to 13:00 PM"
