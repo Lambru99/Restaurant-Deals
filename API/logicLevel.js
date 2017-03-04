7a8661030e84f7821704c0aa56804c3e8c16bfcf
Dumonu <jacobcherry36@gmail.com>
2017-03-04 14:28:46 -0600

    Added address to return type.

--- API/logicLevel.js
+++ API/logicLevel.js
@@ -6,6 +6,7 @@ exports.getRests = function(latitute, longitude, radius, callback)
     restaurants: [
     {
       name: "Bob's Bob Shack",
+      address: "123 Bob Bob Lane"
       image_url: 'http://mylolface.com/assets/faces/happy-smile.jpg',
       distance: 9.3,
       deal: "Free Bobs until the End of Time",
