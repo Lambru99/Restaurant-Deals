757ec117b623d478f9b5d9376540c99fcb99e626
Siddharth Kumar <sidkumar@live.com>
2017-03-05 00:47:26 -0600

    stuff

--- Site/app.js
+++ Site/app.js
@@ -1,17 +1,1 @@
-function hehe(){
 
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', "http://hackjskn.tk/rests/32.985854/-96.747811/10.0", true);
-
-xhr.send();
-xhr.addEventListener("readystatechange", processRequest, false);
-xhr.onreadystatechange = processRequest;
-function processRequest(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var response = JSON.parse(xhr.responseText);
-        alert(response);
-    }
-}
-
-}
